4c0bb2f2c4f9f55837fd13fec667e532150be04a
Ismai21k <ismailkabir170@gmail.com>
2025-03-30 14:47:29 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import './App.css';
 import Nabvar  from './nabvar';
 
 function App() {
-  const name = " Ismail kabir";
-  const age = 23;
-  const country = "Nigeria";
-  const city = "Kaduna";
-  const hobbies = [" Reading", " Writing", " Coding"];
-
   return(
     <div className="App">
       <div className="content">
         <Nabvar />
-        <h1>App component</h1>
-        <h3>Nmae:{name}</h3>
-        <p>Age:{ age}</p>
-        <p>Hobbies:{hobbies}</p>
-        <p>City:{city}</p>
-        <p>Country:{country}</p>
-        <p>{ Math.random() * 10 }</p>
-        <br />
-        <a href="https://learn.microsoft.com/en-gb/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.5" target='blank'>Execution policy</a>
+        <h1>Hello World!</h1>
+        
       </div>
     </div>
   )
