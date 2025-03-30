dd7424daefed3fe282a2277f690c68bc9fce631d
Ismai21k <ismailkabir170@gmail.com>
2025-03-30 19:34:04 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Nabvar  from './nabvar';
 
+// the goal of this project is to create a simple react app
+// that render a simple message Hello World! in h1 tag
+
 function App() {
+ 
   return(
     <div className="App">
       <div className="content">
