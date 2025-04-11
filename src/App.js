197223f5e6d45091e9b3db57ff4776145c5c96e0
Ismai21k <ismailkabir170@gmail.com>
2025-04-11 06:51:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import './App.css';
-import Nabvar  from './nabvar';
+import { useState } from 'react';
 
-// the goal of this project is to create a simple react app
-// that render a simple message Hello World! in h1 tag
+/*the goal of this project is to create a simple react Click counter
+application that allows the user to increase and decrease a count value
+by clicking the respective bttons. the appplication should also include
+a maximum count limit of 10.*/
 
-function App() {
+//requirement: use React functional component with hooks (useState).
+
+  const Increase = () => {
+    
+    const [countValue, setCountValue] = useState(0);
+
+    const handleCount = () => {
+      setCountValue(preCount => {
+        if (preCount >= 10) {
+          alert('You have reached the maximum count');
+          return preCount;
+        }
+
+        return preCount + 1;
+      });
+      
+      
+      
+    };
+
+    const decrease = () => {
+      if (countValue > 0) {
+        setCountValue(countValue - 1)
+      }
+      
+
+
+    };
+    
+    
  
   return(
     <div className="App">
-      <div className="content">
-        <Nabvar />
-        <h1>Hello World!</h1>
-        
-      </div>
+      <div className="content" id='count'>{countValue}</div>
+      <button onClick={handleCount} disabled={countValue > 10}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
     </div>
-  )
+  );
 };
 
-export default App;
+export default Increase;
